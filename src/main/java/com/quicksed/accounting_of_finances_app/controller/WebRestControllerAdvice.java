276e8aed5fca914b60dbcbf26a23a3a283d86116
Quicksed <53088670+Quicksed@users.noreply.github.com>
2021-10-19 18:12:03 +0400

    added vue.js

--- src/main/java/com/quicksed/accounting_of_finances_app/controller/WebRestControllerAdvice.java
+++ src/main/java/com/quicksed/accounting_of_finances_app/controller/WebRestControllerAdvice.java
@@ -3,9 +3,9 @@ package com.quicksed.accounting_of_finances_app.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class WebRestControllerAdvice {
 
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
